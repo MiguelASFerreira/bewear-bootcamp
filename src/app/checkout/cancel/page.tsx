7bae8e645b3ab06663d72045df46b13ce157d76b
MiguelASFerreira <migant1306@gmail.com>
2025-08-10 17:33:30 -0300

    feat: add checkout cancel page

---
+++ src/app/checkout/cancel/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Header } from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const CheckoutSuccessPage = () => {
+  return (
+    <>
+      <Header />
+      <Dialog open={true} onOpenChange={() => {}}>
+        <DialogContent className="text-center">
+          <DialogTitle className="mt-4 text-2xl">Erro ao efetuar o pedido</DialogTitle>
+          <DialogDescription className="font-medium">
+            Houve um erro ao tentar efetuar o pedido. Por favor, tente novamente mais tarde.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="lg"
+              asChild
+            >
+              <Link href="/">Voltar para a loja</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default CheckoutSuccessPage;
